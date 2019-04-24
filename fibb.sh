5d149528931da4235492776ebeb803155cbe03cd
meerasasidharan <48014249+meerasasidharan@users.noreply.github.com>
2019-04-24 15:04:55 +0530

    fibb1.sh commit

--- fibb.sh
+++ fibb.sh
@@ -1,8 +1,15 @@
-echo "enter a number"
-read n
-if [ $n -ge 2 ]
-then
-echo "$n greater than 2"
-else
-echo "not greater than 2"
-fi
+f1=0
+f2=1
+echo " the fibonacci series "
+echo $f1
+echo $f2
+for((i=1;i<=n-2;i++))
+do
+temp=`expr $f1 + $f2`
+f1=$f2
+f2=$temp
+
+ c='echo $f2 | cut -c 3'
+
+done
+ echo $c
